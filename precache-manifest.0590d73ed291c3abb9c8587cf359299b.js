bfc740ade72d0191138fbe15766cb0c36bcaa446
David Piper <black.god1930@gmail.com>
2019-10-14 16:09:28 -0400

    Updates

---
+++ precache-manifest.0590d73ed291c3abb9c8587cf359299b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dd180f870f9705064288016c8bf6595",
+    "url": "/dvd2x/noteful-client.git/index.html"
+  },
+  {
+    "revision": "fbc76c2756a8026f46f6",
+    "url": "/dvd2x/noteful-client.git/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c564676e43e06ab172a8",
+    "url": "/dvd2x/noteful-client.git/static/js/2.fafbc5ab.chunk.js"
+  },
+  {
+    "revision": "fbc76c2756a8026f46f6",
+    "url": "/dvd2x/noteful-client.git/static/js/main.82a1859b.chunk.js"
+  },
+  {
+    "revision": "73bfbf6fc75b4d432219",
+    "url": "/dvd2x/noteful-client.git/static/js/runtime-main.325ba70a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/dvd2x/noteful-client.git/static/media/logo.25bf045c.svg"
+  }
+]);
